bad9b035413d46ef320e49260311b2be1b451a61
tsrk <tsrk@tsrk.me>
2023-12-02 21:33:19 +0100

    feat(day2_1): bootstrap

---
+++ day2_1/submission.c
@@ -0,0 +1,11 @@
+#include "boilerplate.h"
+
+void process_line(char *str)
+{
+    // TODO: implement something
+}
+
+void finish(void)
+{
+    // TODO: implement something
+}
--- dirs.mk
+++ dirs.mk
@@ -1,3 +1,4 @@
 BUILDS = \
 		 day1_1 \
 		 day1_2 \
+		 day2_1 \
